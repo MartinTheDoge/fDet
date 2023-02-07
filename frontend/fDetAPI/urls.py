b9d189d3b66ecabd7d617c25642421de9ec17f1c
TheLastMercy <DavidVrt2@gmail.com>
2023-02-07 19:23:02 +0100

    Jendův main first test na stránce.

--- frontend/fDetAPI/urls.py
+++ frontend/fDetAPI/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, evaluation_view
 
 urlpatterns = [
+    path('evaluation', evaluation_view, name="evaluation"),
     path('', home_view, name="home"),
     path('admin/', admin.site.urls),
     
--- frontend/pages/views.py
+++ frontend/pages/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import sys
+sys.path.append("../backend")
+from main import TextValidate
+
+TEXTVALIDATE_CLASS = TextValidate()
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
     models = {
 
     }
     return render(request, "base.html", models)
+
+def evaluation_view(request, *args, **kwargs):
+    text = request.POST["text"]
+    validated_text = TEXTVALIDATE_CLASS.main(text)
+    
+    context = {
+        "input_text" : text,
+        "validated" : validated_text
+    }
+    return render(request, "base.html", context)
