b504f1dff42fff03c9df9333251a4b9584551a8d
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-02-13 17:57:06 +0100

    ja uz fakt nevim kamo

--- frontend/static/js/real_time.js
+++ frontend/static/js/real_time.js
@@ -1,18 +1,20 @@
 function getInfo() {
     const xhttp = new XMLHttpRequest();
     let text = document.getElementById('eval-input').value;
-    xhttp.open("GET", `/test_view?text=${text}`, true);
+    xhttp.open("GET", `/test_view?text=${text}`, false);
     xhttp.send();
 
-    xhttp.onreadystatechange = () => {
+    xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4) {
             try {
-                let response = xhttp.responseText;
+                let response = JSON.parse(xhttp.responseText);
+                response = JSON.stringify(response);
+                console.log(response)
                 let output_text = document.getElementById('output-text');
                 output_text.innerHTML = response.output;
             } catch (error) {
                 let output_text = document.getElementById('output-text');
-                output_text.innerHTML = error + "\n" + "Error 469 david to zase rozbil :(";
+                output_text.innerHTML = /*error + "\n" +*/ "Error 469 david to zase rozbil :(";
             }
         }
     }
